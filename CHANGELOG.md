d5acac475e3befae5df8a9ff41532fa66ad35680
Nick Gunn <nick@ausimian.net>
2024-03-14 14:23:29 +1100

    Fix format of changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,9 @@
-# 0.1.0 - 14 Mar 2024
+# Changelog
+
+## 0.1.1 - 14 Mar 2024
+
+- Fix format of changelog
+
+## 0.1.0 - 14 Mar 2024
 
 - Initial revision
--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule JsonUrl.MixProject do
     [
       app: :json_url,
       description: "JsonUrl encoding and decoding support",
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
